5062f2a9d6b433206ac09e83da6bee4fa01c61c4
VelislavLeonov <velislavleonov14@gmail.com>
2015-01-24 20:08:21 +0200

    .init

---
+++ HomeworkOperatorsAndExpressions/08.PrimeNumberCheck/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/*Problem 8. Prime Number Check
+
+Write an expression that checks if given positive integer number n (n <= 100) is prime (i.e. it is divisible without remainder only to itself and 1).
+Note: You should check if the number is positive*/
+
+namespace _08.PrimeNumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number lesser or equal to 100");
+            int number = int.Parse(Console.ReadLine());
+            bool check = true;
+            if (number <= 100)
+            {
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                    }
+                }
+                if (check)
+                {
+                    Console.WriteLine("The number is prime");
+                }
+                else
+                {
+                    Console.WriteLine("The number isn't prime");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You are entering invalid number");
+            }
+            
+        }
+    }
+}
---
+++ HomeworkOperatorsAndExpressions/11.BitwiseExtractBit3/BitwiseExtractBit3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*Problem 11. Bitwise: Extract Bit #3
+
+Using bitwise operators, write an expression for finding the value of the bit #3 of a given unsigned integer.
+The bits are counted from right to left, starting from bit #0.
+The result of the expression should be either 1 or 0.*/
+
+namespace BitwiseExtractBit3
+{
+    class BitwiseExtractBit3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter integer number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("The binary representation is:" + Convert.ToString(number, 2).PadLeft(32, '0'));
+            int p = 3;               
+            int mask = 1 << p;        
+            int nAndMask = number & mask;  
+            int bit = nAndMask >> p;  
+            Console.WriteLine(bit);   
+        }
+    }
+}
